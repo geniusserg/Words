e17263cbde1a258e8c24c76f38b8ba50ac401593
geniusserg <danilovsergeydmitrievich@gmail.com.com>
2021-03-12 17:25:52 +0300

    App Core is ready

--- app/src/main/java/com/hse/words/MainActivity.kt
+++ app/src/main/java/com/hse/words/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.hse.words
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
-import java.io.BufferedReader
-import java.io.File
-import java.nio.charset.Charset
 import java.util.*
 import kotlin.collections.HashMap
 import kotlin.random.Random
@@ -26,11 +26,21 @@ class MainActivity : AppCompatActivity() {
     var newWord: Button? = null
     lateinit var dictionary: Set<String>
     lateinit var longWordsDictionary: Array<String>
-    lateinit var longWord: String
+    var longWord: String? = null
+    lateinit var newWordAlert: AlertDialog
+    var listWordsView: ListView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GlobalScope.launch{
+            longWordsDictionary = loadDictionaryAsArray("wordsLong.txt").await()
+            update()
+            runOnUiThread { checkButton?.setEnabled(false) }
+            dictionary = loadDictionaryAsSet("words.txt").await()
+            runOnUiThread { checkButton?.setEnabled(true) }
+        }
         setContentView(R.layout.activity_main)
+        newWordAlertInit()
         buttonMain = findViewById(R.id.word)
         buttonMain?.setText("Loading...")
         newGame = findViewById(R.id.new_game)
@@ -41,15 +51,40 @@ class MainActivity : AppCompatActivity() {
         newGame?.setOnClickListener { update() }
         newWord = findViewById(R.id.new_word)
         newWord?.setEnabled(false)
-        newWord?.setOnClickListener { listWords.add("а") }
+        newWord?.setOnClickListener {
+            newWordAlert.setMessage("Enter new subword from '"+longWord?.toUpperCase(Locale.getDefault())+"' :");
+            newWordAlert.show() }
         exitButton = findViewById(R.id.exit)
         exitButton?.setOnClickListener { this.finishAndRemoveTask() }
-        GlobalScope.launch{
-            dictionary = loadDictionaryAsSet("words.txt").await()
-            longWordsDictionary = loadDictionaryAsArray("wordsLong.txt").await()
-            update()
-        }
-        listWords.add("а")
+        listWordsView = findViewById(R.id.list_words)
+        listWordsView?.setBackgroundColor(Color.GREEN)
+        val newWTmp  = TextView(this)
+        newWTmp.setText("sdgashjawrhwraywertrwtwrtwerwerwer")
+        newWTmp.setTextColor(Color.WHITE)
+        listWordsView?.addFooterView(newWTmp)
+    }
+
+    fun newWordAlertInit(){
+        val alert: AlertDialog.Builder = AlertDialog.Builder(this)
+        alert.setTitle("New Word")
+        alert.setMessage("Enter new subword from :")
+        val input = EditText(this)
+        alert.setView(input)
+        alert.setCancelable(false)
+
+        val alertWarn: AlertDialog = AlertDialog.Builder(this).setTitle("Wrong, try agagin!").create()
+
+        alert.setPositiveButton("Ok", { dialog, whichButton ->
+            val value: String = input.getText().toString().toLowerCase(Locale.getDefault()).replace(" ","")
+            if (!newWord(value)){
+                alertWarn.show()
+            }
+            input.setText("")
+        })
+        alert.setNegativeButton("Cancel", { dialog, whichButton ->
+            dialog.cancel()
+        })
+        newWordAlert = alert.create()
     }
 
     fun loadDictionaryAsSet(resource: String): Deferred<SortedSet<String>>{
@@ -71,11 +106,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun newWord(word: String): Boolean{
-        if (match(word)) {
-            listWords.add(word)
-            return true
+        when (match(word)){
+            true -> {
+                listWords.add(word)
+
+            }
+            false -> return false
         }
-        return false
+        return true
     }
 
     fun update(){
@@ -86,7 +124,8 @@ class MainActivity : AppCompatActivity() {
         score = 0
         GlobalScope.launch {
             longWord = loadRandomWord()
-            runOnUiThread({ buttonMain?.setText(longWord)
+            runOnUiThread({
+                buttonMain?.setText(longWord)
                 newGame?.setEnabled(true)
                 checkButton?.setEnabled(true)
                 newWord?.setEnabled(true)
@@ -109,28 +148,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             runOnUiThread(
-                {newGame?.setEnabled(true)
-                buttonMain?.setText(score.toString())
-                })
+                    {
+                        newGame?.setEnabled(true)
+                        buttonMain?.setText("YUOR SCORE IS : "+score.toString())
+                    })
         }
     }
 
-    fun match(substring : String): Boolean{
+    fun match(substring: String): Boolean{
         if (substring.equals(longWord)){ return false }
         val cMap = HashMap<Char, Int>()
         for (s in substring){
             val temp_count = cMap.get(s)
             if (temp_count != null){
-                cMap.put(s, temp_count+1)
+                cMap.put(s, temp_count + 1)
             }
             else{
                 cMap.put(s, 1)
             }
         }
-        for (s in longWord){
+        for (s in longWord?:""){
             val temp_count = cMap.get(s)
             if (temp_count != null){
-                cMap.put(s, temp_count-1)
+                cMap.put(s, temp_count - 1)
             }
         }
         val result = cMap.filterValues { a -> a > 0 }
